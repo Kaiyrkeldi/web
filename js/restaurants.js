736acb69d6e27fe78b0e92057fef737756727e00
Kaiyrkeldi <67787196+Kaiyrkeldi@users.noreply.github.com>
2021-02-27 20:46:22 +0600

    new

--- js/restaurants.js
+++ js/restaurants.js
@@ -1,12 +1,9 @@
 const anchors = document.querySelectorAll('a[href^="#"]')
 
-// Цикл по всем ссылкам
 for(let anchor of anchors) {
   anchor.addEventListener("click", function(e) {
-    e.preventDefault() // Предотвратить стандартное поведение ссылок
-    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    e.preventDefault()
     const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
-    // Плавная прокрутка до элемента с id = href у ссылки
     document.querySelector(goto).scrollIntoView({
       behavior: "smooth",
       block: "start"
