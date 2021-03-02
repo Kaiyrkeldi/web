0a11cb8043b081315680c8ed6f0f437fb5c8573c
Kaiyrkeldi <67787196+Kaiyrkeldi@users.noreply.github.com>
2021-03-02 13:14:50 +0600

    Update restaurants.js

--- js/restaurants.js
+++ js/restaurants.js
@@ -3,7 +3,7 @@ const anchors = document.querySelectorAll('a[href^="#"]')
 for(let anchor of anchors) {
   anchor.addEventListener("click", function(e) {
     e.preventDefault()
-    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
     document.querySelector(goto).scrollIntoView({
       behavior: "smooth",
       block: "start"
